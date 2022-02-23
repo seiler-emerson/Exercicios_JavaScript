1e959b484f14eba5a19bb4858f86da01b07497df
Emerson <seiler.emerson@gmail.com>
2022-02-23 17:52:24 -0300

    Exercício 83

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 /*
-    Crie um programa onde o usuário digite uma expressão
-    qualquer que use parenteses. Seu programa deverá analisar
-    se a expressão passada está com os parênteses abertos e fechados
-    na ordem correta
+    Crie um programa que armazene o nome e peso de 5 pessoas, depois mostre:
+    * Quantas pessoas foram cadastradas
+    * Uma listagem com a/as pessoas mais pesadas
+    * Uma listagem com a/as pessoas mais leves
 
 */
 
-let expressao = '((a+b)+((c+b))'
-let arrayExpressao = expressao.split('')
-let parentesesAbertos = []
-let parentesesFechados = []
-for (let i = 0; i < expressao.length; i++) {
-    if(arrayExpressao[i] === '(') {
-        parentesesAbertos.push('(')
-    } else if (arrayExpressao[i] === ')') {
-        parentesesFechados.push(')')
+let pessoas = [
+    {
+        nome: 'Ana',
+        peso: 75.5
+    },
+    {
+        nome: 'Pedro',
+        peso: 89
+    },
+    {
+        nome: 'Joana',
+        peso: 89
+    },
+    {
+        nome: 'Carolina',
+        peso: 55
+    },
+    {
+        nome: 'Bianca',
+        peso: 55
     }
-}
+]
 
-if (parentesesAbertos.length === parentesesFechados.length) {
-    console.log("Sua expressão está válida!")
-} else {
-    console.log("Sua expressão está errada!")
-}
+let pessoaMenorPeso = []
+let menorPeso = pessoas[0].peso
 
-console.log(parentesesAbertos)
-console.log(parentesesFechados)
+for (let i = 0; i < pessoas.length; i++) {
+    if(pessoas[i].peso <= menorPeso) {
+        menorPeso = pessoas[i].peso
+        pessoaMenorPeso.pop()
+        pessoaMenorPeso.push(pessoas[i].nome)
+    }
+
+}
+console.log(pessoaMenorPeso)
+console.log(menorPeso)
+
+console.log("")
+console.log("")
+console.log("")
+console.log(`Ao todo foram cadastradas ${pessoas.length} pessoas.`)
+console.log(`O maior peso foi de ${pessoas.length}kg. Peso de XXX XXX.`)
+console.log(`O menor peso foi de ${pessoas.length}kg. Peso de XXX XXX.`)
